e61fdf8b0d73b952be3b7dfa9cc4cfaa703a07da
Brian McShane <brian.mcshane06@gmail.com>
2025-11-12 13:33:35 -0700

    Deleted unneeded js file

--- Resetcode.js
+++
@@ -1,9 +0,0 @@
-// Reset the entire form to its default (empty) state
-document.getElementById("bmiForm").reset();
-
-// Hide and clear the result area
-    {
-    var result = document.getElementById("result");
-      result.textContent = "";
-      result.style.display = "none"; // hide the result box
-}
