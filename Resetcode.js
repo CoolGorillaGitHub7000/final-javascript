7f72fd2c4e6ea4ec4f1377abc304e6934d62e5ca
Brian McShane <brian.mcshane06@gmail.com>
2025-11-12 13:03:37 -0700

    Added the {}

---
+++ Resetcode.js
@@ -0,0 +1,9 @@
+// Reset the entire form to its default (empty) state
+document.getElementById("bmiForm").reset();
+
+// Hide and clear the result area
+    {
+    var result = document.getElementById("result");
+      result.textContent = "";
+      result.style.display = "none"; // hide the result box
+}
